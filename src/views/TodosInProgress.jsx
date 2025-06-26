0d614761fccaf9353fdfae2b28474683d7f70b58
Gideon <kamaugedion95@gmail.com>
2025-06-26 11:03:18 +0300

    Create TodosInProgress

--- src/views/TodosInProgress.jsx
+++ src/views/TodosInProgress.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import TodoItem from "../components/TodoItem";
+import { useTodos } from "../components/context/TodosContext";
+
 export default function TodosInProgress() {
-    return (
-        <div>
-           In Progresss
-        </div>
-    )
+  const { todos } = useTodos();
+
+  const inProgressTodos = todos.filter((todo) => !todo.completed);
+  console.log("In Progress Todos: ", inProgressTodos);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-amber-700">Todos in Progress</h2>
+      {inProgressTodos.length === 0 ? (
+        <p className="text-gray-500 italic">No tasks in progress.</p>
+      ) : (
+        inProgressTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleUpdate={() => {}}
+            handleDeleteTodo={() => {}}
+          />
+        ))
+      )}
+    </div>
+  );
 }
