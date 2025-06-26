43da556dc397257742f5a2ed8192dcc556317eec
Gideon <kamaugedion95@gmail.com>
2025-06-26 11:01:28 +0300

    Create CompletedTodo.jsx

--- src/views/CompletedTodos.jsx
+++ src/views/CompletedTodos.jsx
@@ -1,7 +1,27 @@
+import React from "react";
+import TodoItem from "../components/TodoItem";
+import { useTodos } from "../components/context/TodosContext";
+
 export default function CompletedTodos() {
-    return (
-        <div>
-            Completed Todos
-        </div>
-    )
+  const { todos } = useTodos();
+
+  const completedTodos = todos.filter((todo) => todo.completed);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4 text-amber-700">Completed Todos</h2>
+      {completedTodos.length === 0 ? (
+        <p className="text-gray-500 italic">No completed tasks.</p>
+      ) : (
+        completedTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleUpdate={() => {}}
+            handleDeleteTodo={() => {}}
+          />
+        ))
+      )}
+    </div>
+  );
 }
